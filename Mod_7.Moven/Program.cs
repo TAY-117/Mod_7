3a3b26c2c88d43ddf7e6f406671b9a644ef730b1
TAY-117 <belchack@gmail.com>
2021-06-04 18:45:17 +0300

    Созданы классы  Player, Wolf, Bear, Rock

--- Mod_7.Moven/Program.cs
+++ Mod_7.Moven/Program.cs
@@ -2,27 +2,22 @@
 
 namespace Mod_7.Moven
 {
-	struct Location	// месторасположение
+	// Location - месторасположение
+	struct Location
 	{
-		Location(int x, int y)
-		{
-			X = x;
-			Y = y;
-		}
+		Location(int x, int y) { X = x; Y = y; }
+		internal int X { set; get; }
 
-		internal int X // координата x не может быть за границами поля
-		{ set; get; }
-
-		internal int Y // координата y не может быть за границами поля
-		{ set; get; }
-
+		internal int Y { set; get; }
 	}
 
 	//-----------------------------------------------------------------------------
-	enum Entitys : byte   // перечисление возможных сущностей, размещаемых на поле
+
+	// Entitys - перечисление возможных сущностей, размещаемых на поле
+	enum Entitys : byte
 	{
 		none = 0,   // ничего, никто
-		play,			// игрок
+		player,		// игрок
 		rock,			// камень
 		stump,		// пень
 		apple,		// яблоня
@@ -33,7 +28,19 @@ namespace Mod_7.Moven
 
 	//-----------------------------------------------------------------------------
 
-	class Field // игровое поле с размерами Width (0...x_max) и Height (0...y_max)
+	// Direction - возможные направления перемещения по полю
+	enum Direction : byte
+	{
+		west = 0,   // на Запад,	X+
+		north,      // на Север,	Y+
+		east,       // на Восток,	X-
+		south       // на Юг,		Y-
+	}
+
+	//-----------------------------------------------------------------------------
+
+	// Field - игровое поле с размерами Width (0...x_max) и Height (0...y_max)
+	class Field
 	{
 		private static int width;
 		private static int height;
@@ -61,7 +68,7 @@ namespace Mod_7.Moven
 			get { return y_max; }
 		}
 
-		internal Field(int width, int height )
+		internal Field(int width, int height)
 		{
 			Width = width;
 			Height = height;
@@ -74,41 +81,50 @@ namespace Mod_7.Moven
 			}
 		}
 
-		internal static void Memory(int x, int y, Entitys ent)  // запоминает что за нечто в ячейке поля
+		// Field.Memory(x, y, ent) - запоминает что за нечто в ячейке поля (локации)
+		internal static void Memory(int x, int y, Entitys ent)
 		{
 			array_entitys[x, y] = ent;
 		}
-		internal static void Memory(Location xy, Entitys ent)  // запоминает что за нечто в ячейке поля
+
+		// Field.Memory(xy, ent) - запоминает что за нечто в ячейке поля (локации)
+		internal static void Memory(Location xy, Entitys ent)
 		{
 			array_entitys[xy.X, xy.Y] = ent;
 		}
 
-		internal static void Forget(int x, int y)  // забывает что за нечто было в ячейке поля
+		// Field.Forget(x, y) - забывает что за нечто было в ячейке поля (локации)
+		internal static void Forget(int x, int y)
 		{
 			array_entitys[x, y] = Entitys.none;
 		}
 
-		internal static void Forget(Location xy)  // забывает что за нечто было в ячейке поля
+		// Field.Forget(xy) - забывает что за нечто было в ячейке поля (локации)
+		internal static void Forget(Location xy)
 		{
 			array_entitys[xy.X, xy.Y] = Entitys.none;
 		}
 
-		internal static bool IsFreeLocation(int x, int y)  // возвращает true если локация xy свободна
+		// Field.IsFreeLocation(x, y) - возвращает true если ячейка поля (локация) свободна
+		internal static bool IsFreeLocation(int x, int y)
 		{
 			return array_entitys[x, y] == Entitys.none;
 		}
 
-		internal static bool IsFreeLocation(Location xy)   // возвращает true если локация xy свободна
+		// Field.IsFreeLocation(xy) - возвращает true если ячейка поля (локация) свободна
+		internal static bool IsFreeLocation(Location xy)
 		{
 			return array_entitys[xy.X, xy.Y] == Entitys.none;
 		}
 
-		internal static Entitys WhoInLocation(int x, int y)  // возвращает Entitys из заданной локации xy
+		// Field.WhoInLocation(x, y) - возвращает Entitys из заданной ячейки поля (локации)
+		internal static Entitys WhoInLocation(int x, int y)
 		{
 			return array_entitys[x, y];
 		}
 
-		internal static Entitys WhoInLocation(Location xy)   // возвращает Entitys из заданной локации xy
+		// Field.WhoInLocation(xy) - возвращает Entitys из заданной ячейки поля (локации)
+		internal static Entitys WhoInLocation(Location xy)
 		{
 			return array_entitys[xy.X, xy.Y];
 		}
@@ -136,27 +152,33 @@ namespace Mod_7.Moven
 			loc = xy;
 			Field.Memory(loc, Iam());
 		}
+		internal static int SetMinMax(int min, int num, int max)
+		{
+			if (num < min)
+			{ num = min; }
+			else
+			{
+				if (num > max)
+				{ num = max; }
+			}
+			return num;
+		}
 
-		internal virtual Entitys Iam() { return Entitys.none; } // сообщает, что это такое
+		// Entity.Iam() - сообщает, что это оно такое
+		internal virtual Entitys Iam() { return Entitys.none; }
 
-		internal virtual void Show() { }	// отобразить нечто на поле
+		// Entity.Show() - отображает нечто на поле
+		internal virtual void Show() { }
 
-		internal virtual void Hide() { } // скрыть нечто на поле (оно становится невидимым, но продолжает ещё существовать)
+		// Entity.Hide() - скрывает нечто на поле (оно становится невидимым, но продолжает ещё существовать)
+		internal virtual void Hide() { }
 
-		internal virtual void Done()  // "удалить" нечто с поля (оно становится невидимым, стирается из памяти поля)
+		// Entity.Done() - "удаляет" нечто с поля (оно становится невидимым, стирается из памяти поля)
+		internal virtual void Done()
 		{
 			Hide();
 			Field.Forget(loc);
 		}
-	}
-
-	//-----------------------------------------------------------------------------
-	enum Direction : byte   // возможные направления перемещения по полю
-	{
-		west = 0,   // на Запад,	X+
-		north,      // на Север,	Y+
-		east,       // на Восток,	X-
-		south       // на Юг,		Y-
 	}
 
 	//-----------------------------------------------------------------------------
@@ -200,19 +222,23 @@ namespace Mod_7.Moven
 			this.dir = dir;
 		}
 
-		internal virtual void DecreaseTime()	// обратный счётчик времени, как станет < 0, так будет присвоено time_max в сеттере
+		// Creature.DecreaseTime() - обратный счётчик времени, как станет < 0, так будет присвоено time_max в сеттере
+		internal virtual void DecreaseTime()	
 		{
 			Time--;
 		}
 
-		internal virtual Direction GetDirLeft() // направление влево от текущего
+		// Creature.GetDirLeft() - возвращает направление влево от текущего
+		internal virtual Direction GetDirLeft()
 		{
 			int i_dir = (int)Dir;
 			i_dir++;
 			if (i_dir > 3) { i_dir = 0; }
 			return (Direction)i_dir;
 		}
-		internal virtual Direction GetDirRight() // направление вправо от текущего
+
+		// Creature.GetDirRight() - возвращает направление вправо от текущего
+		internal virtual Direction GetDirRight()
 		{
 			int i_dir = (int)Dir;
 			i_dir--;
@@ -220,14 +246,16 @@ namespace Mod_7.Moven
 			return (Direction)i_dir;
 		}
 
-		internal virtual void Rotate(Direction new_dir)	// тварь ориентируется в новое направление (поворачивается)
+		// Creature.Rotate(new_dir) - тварь ориентируется в новое направление (поворачивается)
+		internal virtual void Rotate(Direction new_dir)
 		{
 			Hide();
 			Dir = new_dir;
 			Show();
 		}
 
-		internal virtual void RotateRandom() // тварь поворачивает на 90 градусов в случайную сторону
+		// Creature.RotateRandom() - тварь поворачивает на 90 градусов в случайную сторону
+		internal virtual void RotateRandom()
 		{
 			Random rnd = new Random();
 			if (rnd.Next(2) == 0)				// случайное число 0 или 1
@@ -235,7 +263,8 @@ namespace Mod_7.Moven
 			else { Rotate(GetDirRight()); }
 		}
 
-		internal virtual Location GetLocInDirection(Direction in_dir)	// возвращает соседнюю локацию в заданном направлении
+		// Creature.GetLocInDirection(in_dir) - возвращает соседнюю локацию в заданном направлении
+		internal virtual Location GetLocInDirection(Direction in_dir)
 		{
 			Location xy = new Location();
 			switch (in_dir)
@@ -257,11 +286,7 @@ namespace Mod_7.Moven
 					xy.Y = loc.Y - 1;
 					break;
 			}
-			return xy;
-		}
-
-		internal virtual void NormLocation(ref Location xy)  // нормализует локацию - если выскочили за край поля, то появляемся с противоположной стороны
-		{
+			// нормализует локацию - если выскочили за край поля, то появляемся с противоположной стороны
 			if (xy.X > Field.X_max)
 			{ xy.X -= Field.Width; }
 			else
@@ -271,29 +296,33 @@ namespace Mod_7.Moven
 			{ xy.Y -= Field.Height; }
 			else
 			{ if (xy.Y < 0) { xy.Y += Field.Height; } }
+			return xy;
 		}
 
-		internal virtual void JumpTo(Location xy)   // перескок в заданную локацию, если вылетаем за край поля, то появляемся с противоположной стороны
+		// Creature.JumpTo(xy) - перескок в заданную локацию, если вылетаем за край поля, то появляемся с противоположной стороны
+		internal virtual void JumpTo(Location xy)
 		{
 			Field.Forget(loc);
 			Hide();
 			loc = xy;
 			Show();
 			Field.Memory(loc, Iam());
-
 		}
-		internal virtual void JumpTo(int x, int y)  // вариант перескока в заданную локацию по координатам
+
+		// Creature.JumpTo( x, y) - вариант перескока в заданную локацию по координатам
+		internal virtual void JumpTo(int x, int y)
 		{
 			Location xy = new Location { X = x, Y = y };
 			JumpTo(xy);
 		}
 
-		internal virtual void Move()	// если время подошло, то совершаем перемещение в соседнюю локацию в текущем направлении
-		{										// при наличии там свободного места, иначе делаем поворот в произвольную сторону на 90 градусов
+		// Creature.Move() - если время подошло, то совершаем перемещение в соседнюю локацию в текущем направлении
+		//							при наличии там свободного места, иначе делаем поворот в произвольную сторону на 90 градусов
+		internal virtual void Move()
+		{
 			if (Time == 0)
 			{
 				Location xy = GetLocInDirection(Dir);
-				NormLocation(ref xy);
 				if (Field.IsFreeLocation(xy))
 				{ JumpTo(xy); }
 				else
@@ -307,37 +336,45 @@ namespace Mod_7.Moven
 
 	abstract class Predator : Creature // хищник
 	{
-		private protected int path_max;  // максимальное расстояние, которое проходит хищник до смены направления движения
-		private protected int path;      // путь, который осталось пройти до смены направления движения
+		private protected int path_max;	// максимальное расстояние, которое проходит хищник до смены направления движения
+		private protected int path;		// путь, который осталось пройти до смены направления движения
 
-		internal virtual int Path { set; get; } // пока простейший вариант для хищника, потом будет сложнее
+		internal virtual int Path { set; get; }	// пока простейший вариант для хищника, потом будет сложнее
 		
 		internal Predator() : base()
 		{
-
+			path_max = 8;
+			path = path_max;
 		}
 
-		internal Predator(int x, int y, int time_max, Direction dir) : base(x, y, time_max, dir)
+		internal Predator(int x, int y, int time_max, Direction dir, int path_max) : base(x, y, time_max, dir)
 		{
-
+			this.path_max = path_max;
+			path = path_max;
 		}
-		internal Predator(Location xy, int time_max, Direction dir) : base(xy, time_max, dir)
+
+		internal Predator(Location xy, int time_max, Direction dir, int path_max) : base(xy, time_max, dir)
 		{
-
+			this.path_max = path_max;
+			path = path_max;
 		}
 
-		internal virtual void DecreasePath()	// обратный счётчик пройденного пути
+		// Predator.DecreasePath() - обратный счётчик пройденного пути
+		internal virtual void DecreasePath()
 		{
 			Path--;
 		}
 
-		internal virtual void NewPath()	// новый путь
+		// Predator.NewPath() - назначаем новое значение пути в выбранном направлении
+		internal virtual void NewPath()
 		{
 			Random rnd = new Random();
-			Path = rnd.Next(1, path_max);	// назначаем новое значение пути в выбранном направлении
+			Path = rnd.Next(1, path_max);
 		}
 
-		internal override void Move()		// если время подошло, то совершаем перемещение в соседнюю локацию в текущем направлении
+		// Predator.Move() - если время подошло, то совершаем перемещение в соседнюю локацию в текущем направлении
+		//							при наличии там свободного места, иначе делаем поворот в произвольную сторону на 90 градусов
+		internal override void Move()
 		{
 			if (Time == 0)
 			{
@@ -346,7 +383,6 @@ namespace Mod_7.Moven
 				else
 				{
 					Location xy = GetLocInDirection(Dir);
-					NormLocation(ref xy);
 					if (Field.IsFreeLocation(xy))
 					{ JumpTo(xy); DecreasePath(); }	// перемещаемся и уменьшаем оставшийся путь
 					else
@@ -356,19 +392,18 @@ namespace Mod_7.Moven
 			DecreaseTime();
 		}
 
-		internal virtual void Action()   // реализация логики действий хищника по поиску жертвы
+		// Predator.Action() - реализация логики действий хищника по поиску жертвы
+		internal virtual void Action()
 		{
 			bool is_find;
 			Location xy = GetLocInDirection(Dir);
-			NormLocation(ref xy);
-			if (is_find = (Field.WhoInLocation(xy) == Entitys.play))
-			{ }   // съедает игрока
+			if (is_find = (Field.WhoInLocation(xy) == Entitys.player))
+			{ }   // съедает игрока, ссылку на которого надо как-то найти
 			else
 			{
 				Direction inspect_dir = GetDirLeft();
 				xy = GetLocInDirection(inspect_dir);
-				NormLocation(ref xy);
-				if (is_find = (Field.WhoInLocation(xy) == Entitys.play))
+				if (is_find = (Field.WhoInLocation(xy) == Entitys.player))
 				{
 					Rotate(inspect_dir);
 				}   // съедает игрока
@@ -376,8 +411,7 @@ namespace Mod_7.Moven
 				{
 					inspect_dir = GetDirRight();
 					xy = GetLocInDirection(inspect_dir);
-					NormLocation(ref xy);
-					if (is_find = (Field.WhoInLocation(xy) == Entitys.play))
+					if (is_find = (Field.WhoInLocation(xy) == Entitys.player))
 					{
 						Rotate(inspect_dir);
 					}   // съедает игрока
@@ -388,8 +422,127 @@ namespace Mod_7.Moven
 
 	}
 
+	//=============================================================================
+	class Player : Creature // игрок
+	{
+		private protected int vital_max;	// максимальное уровень жизненности
+		private protected int vital;		// текщий уровень жизненности
 
-	//-----------------------------------------------------------------------------
+		internal virtual int Vital
+		{
+			set
+			{
+				vital = SetMinMax(0, value, vital_max);
+			}
+			get { return vital; }
+		}
+
+		internal Player() : base()
+		{
+			vital_max = 2;
+			vital = vital_max;
+		}
+
+		internal Player(int x, int y, int time_max, Direction dir, int vital_max) : base(x, y, time_max, dir)
+		{
+			this.vital_max = vital_max;
+			vital = vital_max;
+		}
+
+		internal Player(Location xy, int time_max, Direction dir, int vital_max) : base(xy, time_max, dir)
+		{
+			this.vital_max = vital_max;
+			vital = vital_max;
+		}
+
+		// Player.Move() - перемещение игрока
+		internal override void Move()
+		{
+			if (Time == 0)
+			{
+				Location xy = GetLocInDirection(Dir);
+				if (Field.IsFreeLocation(xy))
+					{ JumpTo(xy); }  // перемещаемся
+			}
+			DecreaseTime();
+		}
+
+		// Player.Control() - управление направлением перемещения игрока
+		internal virtual void Control()
+		{
+			// по управляющим клавишам назначается Direction new_dir и выполнятеся Rotate(new_dir);
+			// и где-то должен вызываться метод Move(), но вот где... ?
+
+		}
+
+		// Player.Search() - если нашли что-нибудь сьедобное, то съедаем и увеличиваем жизненность
+		internal virtual void Search()
+		{
+			Location xy = GetLocInDirection(Dir);
+			Entitys ent = Field.WhoInLocation(xy);
+			if (ent == Entitys.apple || ent == Entitys.pear)
+			{
+				Vital = Vital + 1; // заменить 1 на возвращаемое деревом количество жизненности
+				// и как-то должна у дерева сняться эта жизненность, как определить у какого, ссылки ведь на него нет...
+			}
+		}
+	}
+
+	//=============================================================================
+
+	class Wolf : Predator // волк
+	{
+		internal Wolf(int x, int y, int time_max, Direction dir, int path_max) : base(x, y, time_max, dir, path_max)
+		{ }
+
+		internal Wolf(Location xy, int time_max, Direction dir, int path_max) : base(xy, time_max, dir, path_max)
+		{ }
+
+		// Wolf.Iam() - сообщает, что это волк
+		internal override Entitys Iam() { return Entitys.wolf; }
+
+		// Wolf.Show() - отображает волка на поле
+		internal override void Show() { }
+
+		// Wolf.Hide() - скрывает волка на поле (он становится невидимым, но продолжает ещё существовать)
+		internal override void Hide() { }
+	}
+
+	//=============================================================================
+	class Bear : Predator // ведмедь
+	{
+		internal Bear(int x, int y, int time_max, Direction dir, int path_max) : base(x, y, time_max, dir, path_max)
+		{ }
+
+		internal Bear(Location xy, int time_max, Direction dir, int path_max) : base(xy, time_max, dir, path_max)
+		{ }
+
+		// Bear.Iam() - сообщает, что это волк
+		internal override Entitys Iam() { return Entitys.bear; }
+
+		// Bear.Show() - отображает волка на поле
+		internal override void Show() { }
+
+		// Bear.Hide() - скрывает волка на поле (он становится невидимым, но продолжает ещё существовать)
+		internal override void Hide() { }
+	}
+
+	//=============================================================================
+
+	class Rock : Entity	// камень
+	{
+		internal Rock(int x, int y) : base(x, y)
+		{ }
+
+		internal Rock(Location xy) : base(xy)
+		{ }
+
+		// Rock.Iam() - сообщает, что это камень
+		internal override Entitys Iam() { return Entitys.rock; }
+
+		// Rock.Show() - отображает камень на поле
+		internal override void Show() { }
+	}
 
 	class Program
 	{
